39cc5b689d0912a5281cc4f464fabfa3ab031a59
dmann3134 <dmann3134@gmail.com>
2020-03-29 11:13:27 -0700

    Init

---
+++ Source/Codzilla/CodzillaGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CodzillaGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CODZILLA_API ACodzillaGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
